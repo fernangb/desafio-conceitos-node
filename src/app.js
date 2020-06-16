77f0cf2d230e8dbfd653d00287febe9454a8b165
Gabriel Fernandes <gfsouza@poli.ufrj.br>
2020-06-15 21:56:19 -0300

    CRUD for repository

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,101 @@ app.use(cors());
 
 const repositories = [];
 
+function logRequests(request,response, next){
+  const {method, url} = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+function validateRepositoryId(request, response, next){
+  const {id} = request.params;
+
+  if(!isUuid()){
+      return response.status(400).json({error: 'This is an invalid ID'});
+  }
+  
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, owner, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    owner,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title, owner, techs} = request.body;
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex<0){
+      return response.status(400).json({error: "Invalid repository"});
+  }
+
+  const repository = {
+      id,
+      title,
+      owner,
+      techs,
+      likes: repositories[repositoryIndex].likes
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex<0){
+      return response.status(400).json({error: "Invalid repository"});
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex<0){
+      return response.status(400).json({error: "Invalid repository"});
+  }
+
+  const repository = {
+    id,
+    title: repositories[repositoryIndex].title,
+    owner: repositories[repositoryIndex].owner,
+    techs: repositories[repositoryIndex].techs,
+    likes: repositories[repositoryIndex].likes + 1,   
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
+
 
 module.exports = app;
